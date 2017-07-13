4b2e020ad4a1962d9d9d435bb2238a69d28f9999
Ash Davies <ash.davies@gmail.com>
2017-07-13 14:18:53 +0200

    Remove Stetho

--- mobile/src/main/kotlin/de/scout/fireplace/Fireplace.kt
+++ mobile/src/main/kotlin/de/scout/fireplace/Fireplace.kt
@@ -2,17 +2,12 @@ package de.scout.fireplace
 
 import android.content.Context
 import android.support.multidex.MultiDex
-import com.facebook.stetho.Stetho
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import de.scout.fireplace.inject.ApplicationModule
 import de.scout.fireplace.inject.DaggerApplicationComponent
 
 class Fireplace : DaggerApplication() {
-  override fun onCreate() {
-    super.onCreate()
-    Stetho.initializeWithDefaults(this);
-  }
 
   override fun attachBaseContext(base: Context) {
     super.attachBaseContext(base)
