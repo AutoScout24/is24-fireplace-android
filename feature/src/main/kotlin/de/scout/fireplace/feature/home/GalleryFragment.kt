4bde72f7966aa6e0f483a37eeed06adf8d361a0e
Ash Davies <ash.davies@gmail.com>
2017-09-18 23:31:24 +0200

    Show match fragment as full screen

--- feature/src/main/kotlin/de/scout/fireplace/feature/home/GalleryFragment.kt
+++ feature/src/main/kotlin/de/scout/fireplace/feature/home/GalleryFragment.kt
@@ -21,12 +21,20 @@ class GalleryFragment : DaggerAppCompatDialogFragment() {
     return inflater.inflate(R.layout.fragment_gallery, container, false)
   }
 
-  fun bind(pictures: List<Expose.Picture>) {
-    this.adapter = GalleryAdapter(pictures, { position -> reporting.scroll(position) })
-  }
-
   override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+
+    activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
     pager.adapter = adapter
+  }
+
+  fun bind(pictures: List<Expose.Picture>) {
+    this.adapter = GalleryAdapter(pictures, { position -> reporting.scroll(position) })
   }
 }
